ee7537a392e8221d175189820a533adc21cb3982
Ian <ian.were@student.moringaschool.com>
2023-10-06 00:12:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name,email) {
+const formData = {
+    userName: "name",
+    userEmail:"email",
+};
+
+const configurationObject= {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+    },
+    body: JSON.stringify(formData),
+};
+fetch("http://localhost:3000/users",configurationObject)
+   .then(function (response) {
+    return response.json();
+   })
+   .then(function (object) {
+    console.log(object);
+   })
+   .catch(function (error) {
+    alert("invalid info");
+    console.log(error.message);
+   });
+}
